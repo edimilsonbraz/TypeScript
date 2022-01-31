62f7fcd47e31362c9059b0d4c37762500aca17b8
edimilsonbraz <edimilson.gt8@gmail.com>
2022-01-31 17:21:07 -0300

    Introduçao ao TypeScript & O Desafio 1 finalizado

---
+++ 1_Introducao/desafio1.js
@@ -0,0 +1,6 @@
+var num1 = 5;
+var num2 = 4;
+function getNumbers(n1, n2) {
+    console.log(n1 + n2);
+}
+getNumbers(num1, num2);
---
+++ 1_Introducao/desafio1.ts
@@ -0,0 +1,8 @@
+const num1 = 5
+const num2 = 4
+
+function getNumbers(n1, n2: number) {
+  console.log(n1 + n2)
+}
+
+getNumbers(num1, num2)
